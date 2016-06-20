d4981a277805fd14ee40c2bcf5cac2db21b3ea5f
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-06-20 15:32:13 +0200

    Funziona

---
+++ src/data/generic.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2016 Matteo Alessio Carrara <sw.matteoac@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+
+import all_phrases
+
+
+phrases =	[
+				"TANTI SALUMI.. . SIO GIUSEPPO",
+				"A questo punto ci andrebbe un altra guerra dei sei giorni in modo da occupare altri pezzi di giordania, siria e libano",
+				"D'altra parte nelle guerre si perde e si vince",
+				"La mia ragazza è ballerina e ha lavorato in diverse trasmissioni in tv",
+				"Mi alleno a fare pompini con l'alluce del mio piede sinistro",
+				"Quando i miei figli lo hanno scoperto (14 anni e 16) mi hanno fatto i complimenti per i 20k",
+				"DUX MEA LUX",
+				"Pier de' Fessini, borgomastro uscente dall'Augusta urbe Taurina, est, fra costoro, lo profeta più efficace, colui che riuscì a predìr l'impredicibile"
+				"In anno millequatrocentoventiduesimo dalla fruttifera incarnazione di Nostro Signore, questi, dinanzi "\
+				"allo spauracchio ottomano appena sgominato e quindi lungi dalle mura bizantine, si fece beffe "\
+				"delle forze turche, sfidandole ancormente ad assediar la magna urbe del Bosforo"
+
+			]
+
+
+all_phrases.add_phrases(phrases)
+
---
+++ src/data/informatica.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2016 Matteo Alessio Carrara <sw.matteoac@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+
+import all_phrases
+
+
+phrases =	[
+				"Lo sanno tutti che arch è instabile",
+				"Per l'onion scanner pensavo di aggiungere una caratteristica che potrebbe duplicare, se non triplicare o quadruplicare, a seconda, le possibilità di trovare qualcosa",
+				"Magari partendo da un singolo scanner che è in grado ti testare 5 combinazioni contemporaneamente, per esempio",
+				"Dopo di che, gli URL trovati vengono salvati in un file .log, con tanto di HTTP HEADER, nel nostro server",
+				"Ovviamente se è ottimizzato e non si mette a fare leak vari",
+				"Continuo a pensare che sia solo una perdita di tempo",
+			]
+
+
+all_phrases.add_phrases(phrases)
+
